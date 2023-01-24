564bdf1e67105642731d2eae7137f0e610c89304
SerialEditor <refservicesamara@yandex.ru>
2023-01-24 23:20:44 +0400

    Добавляет блок фильтрации изображений на сайте

--- scripts/layout-mode-toggler.js
+++ scripts/layout-mode-toggler.js
@@ -1,10 +1,12 @@
 'use strict';
 
 let layoutSetButton = document.querySelector('.layout--set');
+let layoutModeText = layoutSetButton.querySelector('.layout-mode');
 
 layoutSetButton.addEventListener('click', function () {
     previewList.classList.toggle('preview__list--tile-layout');
     if (previewList.classList.contains('preview__list--tile-layout')) {
         layoutSetButton.setAttribute('style', '--set-opacity: 1;');
-    } else {layoutSetButton.removeAttribute('style');}
+        layoutModeText.textContent = 'Плитка';
+    } else {layoutSetButton.removeAttribute('style'); layoutModeText.textContent = 'Ряд';}
 });
