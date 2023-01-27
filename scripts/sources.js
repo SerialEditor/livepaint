61900457eac7786e0e05c2b23b851319dc0ef1c8
SerialEditor <refservicesamara@yandex.ru>
2023-01-27 17:33:07 +0400

    Рефакторит функции в sources.js

--- scripts/sources.js
+++ scripts/sources.js
@@ -6,22 +6,24 @@ let previewList = document.querySelector('.preview__list');
 let previews = previewList.querySelectorAll('.preview__image');
 let sliderImage = document.querySelector('.slider__image');
 
-function getInitSlideSource(initSlide) {
-    let splitSliderImageSource = initSlide.attributes.src.textContent.split('/');
-    let conjointSliderImageWay = splitSliderImageSource.slice([0], [splitSliderImageSource.length - 1]).join('/');
-    return conjointSliderImageWay;
+function getInitSlideSourcePath(initSlide) {
+    let splitSource = initSlide.attributes.src.textContent.split('/');
+    let selectedPath = splitSource.slice([0], [splitSource.length - 1]);
+    if (selectedPath.length > 1) {
+        return selectedPath.join('/') + '/';
+    } return selectedPath + '/';
 }
 
-function getSlideInitSources(initSlideSource) {
+function getSlideInitSources(initPath) {
     let initSlideSources = [...previews].map(function (preview) {
-        let splitPreviewSource = preview.attributes.src.textContent.split('/');
-        let previewName = splitPreviewSource[splitPreviewSource.length - 1];
-        return initSlideSource + '/' + previewName;
+        let splitSource = preview.attributes.src.textContent.split('/');
+        let previewName = splitSource[splitSource.length - 1];
+        return initPath + previewName;
     });
     return initSlideSources;
 }
 
-const initSlideSource = getInitSlideSource(sliderImage);
-let slideSources = getSlideInitSources(initSlideSource);
+const initSlideSourcePath = getInitSlideSourcePath(sliderImage);
+let slideSources = getSlideInitSources(initSlideSourcePath);
 
 
