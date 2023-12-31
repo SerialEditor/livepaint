68a5eb217dcd2c813b5df3065269387851de4ad1
SerialEditor <refservicesamara@yandex.ru>
2023-12-31 21:26:27 +0300

    serve: Добавляет webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: {
+    index: path.join(__dirname, "source", "index.js"),
+    gallery: path.join(__dirname, "source", "gallery", "index.js"),
+    articles: path.join(__dirname, "source", "articles", "index.js"),
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].[contenthash:8].js",
+    // assetModuleFilename: path.join("assets", "img", "[name][ext]"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "source", "index.html"),
+      chunks: ["index"],
+      filename: path.join(__dirname, "dist", "index.html"),
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "source", "gallery", "index.html"),
+      chunks: ["gallery"],
+      filename: path.join(__dirname, "dist", "gallery", "index.html"),
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "source", "articles", "index.html"),
+      chunks: ["articles"],
+      filename: path.join(__dirname, "dist", "articles", "index.html"),
+    }),
+    new FileManagerPlugin({
+        events: {
+            onStart: {
+                delete: ["dist"],
+            },
+        },
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      // {
+      //   test: /\.(png|jpg|jpeg|gif)$/i,
+      //   type: "asset/resource",
+      // },
+    ],
+  },
+  devServer: {
+    watchFiles: path.join(__dirname, "source"),
+    port: 9000,
+  },
+};
