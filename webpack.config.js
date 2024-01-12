31e5773a0112becca172952a961027e52b608fb9
SerialEditor <refservicesamara@yandex.ru>
2024-01-12 20:00:26 +0300

    feat: Добавляет шрифты в проект

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const FileManagerPlugin = require("filemanager-webpack-plugin");
-const path = require("path");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    index: path.join(__dirname, "source", "pages", "index.js"),
-    gallery: path.join(__dirname, "source", "pages", "gallery", "index.js"),
-    articles: path.join(__dirname, "source", "pages", "articles", "index.js"),
+    index: path.join(__dirname, 'source', 'pages', 'index.js'),
+    gallery: path.join(__dirname, 'source', 'pages', 'gallery', 'index.js'),
+    articles: path.join(__dirname, 'source', 'pages', 'articles', 'index.js'),
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].[contenthash:8].js",
-    assetModuleFilename: path.join("assets", "img", "[name][ext]"),
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[contenthash:8].js',
+    assetModuleFilename: path.join('assets', 'img', '[name][ext]'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "source", "pages", "index.html"),
-      chunks: ["index"],
-      filename: path.join(__dirname, "dist", "index.html"),
+      template: path.join(__dirname, 'source', 'pages', 'index.html'),
+      chunks: ['index'],
+      filename: path.join(__dirname, 'dist', 'index.html'),
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "source", "pages", "gallery", "index.html"),
-      chunks: ["gallery"],
-      filename: path.join(__dirname, "dist", "gallery", "index.html"),
+      template: path.join(__dirname, 'source', 'pages', 'gallery', 'index.html'),
+      chunks: ['gallery'],
+      filename: path.join(__dirname, 'dist', 'gallery', 'index.html'),
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "source", "pages", "articles", "index.html"),
-      chunks: ["articles"],
-      filename: path.join(__dirname, "dist", "articles", "index.html"),
+      template: path.join(__dirname, 'source', 'pages', 'articles', 'index.html'),
+      chunks: ['articles'],
+      filename: path.join(__dirname, 'dist', 'articles', 'index.html'),
     }),
     new MiniCssExtractPlugin(
       {
@@ -39,7 +39,7 @@ module.exports = {
     new FileManagerPlugin({
         events: {
             onStart: {
-                delete: ["dist"],
+                delete: ['dist'],
             },
         },
     }),
@@ -49,18 +49,18 @@ module.exports = {
     rules: [
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
         options: {
           sources: {
             list: [
-              "...",
+              '...',
               {
-                tag: "a",
-                attribute: "href",
-                type: "src",
+                tag: 'a',
+                attribute: 'href',
+                type: 'src',
                 filter: (tag, attribute, attributes) => {
                   const entry = attributes.find(attr => attr.name === attribute);
-                  return entry.value.includes("_slide.jpg");
+                  return entry.value.includes('_slide.jpg');
                 },
               },
             ],
@@ -78,12 +78,19 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|webp)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext]',
+        }
       },
     ],
   },
   devServer: {
-    watchFiles: path.join(__dirname, "source"),
+    watchFiles: path.join(__dirname, 'source'),
     port: 9000,
   },
 };
